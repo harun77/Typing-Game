572f7a87c8b8f21e0625a5f2fa184aa8ca5b21d9
hor <hor@fiftytwo.com>
2020-05-23 19:42:29 +0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from "faker";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing-game';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string){
+    this.enteredText = value;
+  }
+
+  getClassName(first: string, second: string){
+    if(!second) return 'initial';
+    return first === second ? 'matched' : 'not-matched';
+  }
 }
